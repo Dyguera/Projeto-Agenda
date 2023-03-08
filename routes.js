0bf1ac23fc99717c75c878d0f37384d57a48fb6a
Dyguera <tomazetto.cruz@gmail.com>
2023-03-08 09:54:18 -0300

    Cadastro finalizado

--- routes.js
+++ routes.js
@@ -10,5 +10,8 @@ route.post('/', homeController.trataPost);
 // Rota Login e Cadastro
 route.get ('/login/index', loginController.index);
 route.post ('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
 
 module.exports = route; 
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
+const { async } = require('regenerator-runtime');
 
 const LoginSchema = new mongoose.Schema({ 
     email: {type: String, required: true },
@@ -20,12 +22,40 @@ class Login {
         async register(){ 
             this.valida()
             if(this.errors.length>0) return
+
+            await this.userExists()
+            if(this.errors.length>0) return
+
+            const salt = bcryptjs.genSaltSync();
+            this.body.password = bcryptjs.hashSync(this.body.password, salt); 
+
+
             try {
                 this.user = await LoginModel.create(this.body)
             }catch(e){ 
                 console.log(e);
             }
         }
+
+        async login() {
+            this.valida()
+            if(this.errors.length> 0) return;
+            this.user = await LoginModel.findOne({email:this.body.email})
+            if(!this.user){ 
+                this.errors.push("Usuário informado não encontrado");
+                return;
+            }
+            if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+                this.errors.push("Senha incorreta");
+                this.user = null;
+                return;
+            }
+        }
+
+    async userExists(){ 
+        this.user = await LoginModel.findOne({email: this.body.email})
+        if( this.user) this.errors.push('Usuário Já existe.')
+    }
     valida() { 
         this.cleanUp()
         // E-mail // Usando o pacote validator
